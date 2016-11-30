52aee30aacbfad16e87a629e07eaeab1c7b4fcb2
ximet <dq.ximet@gmail.com>
2016-11-30 21:16:15 +0300

    retweet bot was born

---
+++ server.js
@@ -0,0 +1,40 @@
+var twit = require('twit');
+var config = require('./config.js');
+
+var Twitter = new twit(config);
+
+var retweet = function() {
+    var params = {
+        q: '#nodejs, #Nodejs',  // REQUIRED
+        result_type: 'recent',
+        lang: 'en'
+    }
+    Twitter.get('search/tweets', params, function(err, data) {
+      // if there no errors
+        if (!err) {
+          // grab ID of tweet to retweet
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
+        }
+        // if unable to Search a tweet
+        else {
+          console.log('Something went wrong while SEARCHING...');
+        }
+    });
+}
+
+
+retweet();
+// retweet in every 50 minutes
+setInterval(retweet, 3000000); //every 50min
