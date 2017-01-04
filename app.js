05055df23d13127773cc516117484e03bd9c2612
ximet <dq.ximet@gmail.com>
2017-01-04 21:09:47 +0300

    check event line readLineInterface

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const cliClass = require('./cli.js');
 const cli = new cliClass();
 
-cli.startWork();
+// cli.startWork();
+
+const RL = require('./ReadLineInterface.js');
+
+
+RL.on("line", (cmd) => {
+    console.log(cmd);
+});
