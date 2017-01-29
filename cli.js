c1f56b2de4a68115d75798718c8bcbb08a10609e
ximet <dq.ximet@gmail.com>
2017-01-29 14:01:33 +0300

    some minor refactor

--- cli.js
+++ cli.js
@@ -84,51 +84,6 @@ const whichLanguageTranslate = () => {
 class CLI extends EventEmitter {
     constructor() {
         super();
-
-
-        this.stdin = process.stdin;
-        this.stdout = process.stdout;
-
-        this.stdin.resume();
-        this.stdin.setEncoding('utf8');
-
-        this.startListening = this.startListening.bind(this);
-        this.stopListening = this.stopListening.bind(this);
-    }
-
-    startListening () {
-        const self = this;
-        const twitterObject = new TwitterAddons();
-
-        if (isStarted === true) {
-            return;
-        }
-
-        this.stdin.on('data', function (text) {
-            console.log(process.argv);
-            console.log('received data:', util.inspect(text));
-            if (text === 'twit please\n') {
-                twitterObject.tweetedTweet('Yohooo first tweet from bot c2-d2');
-            }
-            if (text === 'quit\n') {
-                self.stopListening();
-            }
-        });
-
-        this.emit('start');
-        isStarted = true;
-        isStopped = false;
-    }
-
-    stopListening () {
-        if (isStopped || !isStarted) {
-            return;
-        }
-
-        this.stdin.destroy();
-        this.emit('stop');
-        isStopped = true;
-        isStarted = false;
     }
 
     commandUser () {
